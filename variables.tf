f906a15bf8a11c7307bb51602d50de255fb8c597
akshow37 <ashokanbi37@gmail.com>
2024-06-05 12:50:11 -0400

    login page

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "my_ip" {
+   description = "My static IP address"
+   type = string
+   sensitive = true
+}
+
+variable "region" {
+   description = "Define the region where AWS is provisioned"
+   default = "us-east-2"
+}
